e4c4b0d796668dc95d52d047d4e165d84efd65bb
Anjali Rawat <rawatanjali0513@gmail.com>
2026-08-16 23:00:12 +0530

    Update 07_JOINS.sql

--- 07_JOINS.sql
+++ 07_JOINS.sql
@@ -140,6 +140,14 @@ c.CustomerName,c.City,
 c.CustomerID;
 
 
+           -- RIGHT JOIN
+-- syntax
+
+-- SELECT columns
+-- FROM table1
+-- RIGHT JOIN table2
+-- ON table1.common_column = table2.common_column
+
 
 
 
