faeb8cd1e55a2ac7be45ba9fb482a187a4abaa73
Anjali Rawat <rawatanjali0513@gmail.com>
2026-08-17 13:16:11 +0530

    Document subquery usage and examples in SQL
    Added comments and basic syntax for subqueries in SQL.

--- 07_subqueries.sql
+++ 07_subqueries.sql
@@ -1,1 +1,38 @@
+--  SUBQUERIES 
+
+-- A subquery is a query written inside another SQL query.
+
+
+/*Basic Syntax
+
+SELECT column_name
+FROM table_name
+WHERE column_name OPERATOR (
+    SELECT column_name
+    FROM table_name
+  WHERE condition
+);
+
+*/
+
+-- Find customers whose salary is greater than the average salary of all customers.
+
+-- Find customers whose salary is equal to the highest salary.
+
+
+-- Find customers whose salary is equal to the lowest salary
+
+-- Check whether the company has more than 1000 customers.
+
+
+-- Find customers who have placed an order.
+
+-- Find customers whose salary is greater than the average salary.
+
+
+-- Find the customer(s) having the maximum salary.
+
+-- Find cities whose average salary is greater than the overall average salary.
+
+-- Find customers whose salary is greater than the average salary of their own city.
 
